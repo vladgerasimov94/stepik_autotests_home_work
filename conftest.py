6df21c44ddbb19a0386ea3ade667c969b61f7ac3
Vladislav Gerasimov <vladgerasimov94@yandex.ru>
2020-02-06 00:47:13 +0300

    Добавил assert

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default="None",
-                    help="Choose language: ru, en, ...")
+                     help="Choose language: ru, en, ...")
 
 
 @pytest.fixture(scope="function")
@@ -15,7 +15,6 @@ def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
 
-    # browser = None
     if browser_name == "chrome":
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
--- test_items.py
+++ test_items.py
@@ -1,11 +1,13 @@
 import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+BUTTON_PATH = ".btn-primary"
+EXPECTED_BUTTON_LEN = 1
 
 
-def test_guest_should_see_button_add_to_basket(browser):
+def test_guest_should_see_button_for_add_to_basket(browser):
     browser.get(link)
-    time.sleep(10)
-    browser.find_element_by_css_selector(".btn-primary")
-
-# Написать внятный ассерт
+    # time.sleep(30)
+    button_len = len(browser.find_elements_by_css_selector(BUTTON_PATH))
+    assert button_len == EXPECTED_BUTTON_LEN, f'Ooops, i can not find button with CSS: \"{BUTTON_PATH}\". ' \
+                                              f'\nExpected: {EXPECTED_BUTTON_LEN} element(s), got {button_len} element(s)'
